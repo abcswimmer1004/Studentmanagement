4be3b075cde8f0896748bfa06110455f5a52dcb2
abcswimmer1004 <abcswimmer1004@gmail.com>
2026-01-07 22:48:31 +0900

    第5回課題途中

--- src/main/java/raisetech/Student/management/Application.java
+++ src/main/java/raisetech/Student/management/Application.java
@@ -1,22 +1,30 @@
 package raisetech.Student.management;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class Application {
 
+	private String name = "Kazuto Tanaka";
+
 	public static void main(String[] args) {
-		// localhost:8080
 		SpringApplication.run(Application.class, args);
 	}
 
+	@GetMapping("/name")
+	public String getName() {
+		return name;
+	}
 
-	@GetMapping("/okinawakou")
-	public String hello(){
-		return  "2026年こそ12勝！";
+	@PostMapping("/name")
+	public void setName(String name){
+		this.name = name;
 	}
 }
+
